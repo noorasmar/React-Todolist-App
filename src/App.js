6724553d5a580d4f14688f4eeecc06c61ae73f87
Noor Asmar <noor_jb@outlook.com>
2022-09-06 15:02:57 +0300

    buil ui screen

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import AddTask from './components/AddForm/index';
+import TaskList from './components/TaskList/index';
 
 
 function App() {
   return (
     <div className="App">
       <AddTask />
+      <TaskList />
     </div>
   );
 }
